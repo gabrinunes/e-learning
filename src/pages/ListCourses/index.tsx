87a7311176debfade8b06d9295d843b4365becf6
Gabriel Cunha <gn_cunha@hotmail.com>
2020-11-30 11:18:35 -0300

    criação da página de listagem de aulas

--- src/pages/ListCourses/index.tsx
+++ src/pages/ListCourses/index.tsx
@@ -1,13 +1,101 @@
 import React from 'react';
 import LessonHeader from '../../components/LessonHeader';
 
-import {Container} from './styles';
+import {
+  Container,
+  LessonName,
+  NumberOfLessons,
+  LessonPlayIcon,
+  LessonCard,
+  NameOfLesson,
+  LessonInfo,
+  LessonStatus,
+  LessonStatusText,
+} from './styles';
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
 
 const ListCourses: React.FC = () => {
+  const DATA = [
+    //Testando Lista
+    {
+      id: 'bd7',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d76472',
+      title: 'Third Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d7892',
+      title: 'Third Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d723',
+      title: 'Third Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d712',
+      title: 'Third Item',
+    },
+  ];
   return (
     <>
       <LessonHeader />
-      <Container />
+      <Container>
+        <LessonName>Matemática</LessonName>
+        <NumberOfLessons>16 aulas</NumberOfLessons>
+        <FlatList
+          data={DATA}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => (
+            <TouchableOpacity>
+              <View style={{flex: 1, alignItems: 'center', marginBottom: 16}}>
+                <LessonCard>
+                  {item.id === 'bd7' ? ( //testando comportamento de cores para descobrir se uma aula foi completa ou não
+                    <LessonPlayIcon isCompleted>
+                      <Icon name="play-circle" size={35} color="#fff" />
+                    </LessonPlayIcon>
+                  ) : (
+                    <LessonPlayIcon>
+                      <Icon name="play-circle" size={35} color="#fff" />
+                    </LessonPlayIcon>
+                  )}
+                  <NameOfLesson>Introdução à teoria matemática</NameOfLesson>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      bottom: 38,
+                      justifyContent: 'space-evenly',
+                      marginLeft: 60,
+                    }}>
+                    <LessonInfo>Aula 01</LessonInfo>
+                    <Icon
+                      name="clock"
+                      size={12}
+                      color="#c4c4d1"
+                      style={{right: 5}}
+                    />
+                    <LessonInfo style={{right: 20}}>5 min</LessonInfo>
+                    {item.id === 'bd7' ? ( //testando comportamento de cores para descobrir se uma aula foi completa ou não
+                      <LessonStatus>
+                        <LessonStatusText>Completo!</LessonStatusText>
+                      </LessonStatus>
+                    ) : (
+                      <LessonStatus style={{backgroundColor: '#FFF'}} /> //testando comportamento de cores para descobrir se uma aula foi completa ou não
+                    )}
+                  </View>
+                </LessonCard>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </Container>
     </>
   );
 };
--- src/pages/ListCourses/styles.ts
+++ src/pages/ListCourses/styles.ts
@@ -1,8 +1,117 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
+
+interface LessonContainerProps {
+  isCompleted?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
+  flex-direction: column;
   background: #f0edf5;
   border-top-left-radius: 24px;
   border-top-right-radius: 24px;
+`;
+
+export const LessonName = styled.Text`
+  font-family: 'Rubik-Regular';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 30px;
+  line-height: 36px;
+
+  /* identical to box height */
+
+  /* Texto / Título */
+  color: #3d3d4c;
+  margin: 23px;
+`;
+
+export const NumberOfLessons = styled.Text`
+  position: absolute;
+  right: 5%;
+  top: 6%;
+  font-family: 'Roboto-Regular';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 18px;
+
+  /* identical to box height */
+  text-align: right;
+
+  /* Texto / Detalhes */
+  color: #a0a0b2;
+`;
+
+export const LessonCard = styled.View`
+  width: 290px;
+  height: 100px;
+  background: #ffffff;
+  border-radius: 16px;
+`;
+
+export const LessonPlayIcon = styled.View<LessonContainerProps>`
+  width: 68px;
+  height: 68px;
+  ${(props) =>
+    props.isCompleted
+      ? css`
+          background: #61c5bd;
+        `
+      : css`
+          background: #ff6680;
+        `}
+  border-radius: 16px;
+  right: 8%;
+  top: 15%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NameOfLesson = styled.Text`
+  font-family: 'Rubik-Regular';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 20px;
+
+  color: #6c6c80;
+
+  align-self: center;
+  bottom: 52%;
+  max-width: 135px;
+`;
+
+export const LessonInfo = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 12px;
+
+  /* identical to box height */
+
+  /* Texto / Inputs */
+  color: #c4c4d1;
+`;
+
+export const LessonStatus = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 63px;
+  height: 16px;
+  background: #61c5bd;
+  border-radius: 12px;
+`;
+
+export const LessonStatusText = styled.Text`
+  font-family: 'Roboto-Medium';
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 10px;
+
+  /* identical to box height, or 100% */
+
+  /* Shapes / Principal */
+  color: #ffffff;
 `;
